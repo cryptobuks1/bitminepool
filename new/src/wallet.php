73dff8744dc2fe9b7a209857c9db18fc19e6d427
unknown <meettomangesh@gmail.com>
2018-08-30 23:27:52 +0530

    updated to save work

--- new/src/wallet.php
+++ new/src/wallet.php
@@ -87,7 +87,7 @@
                                             ?>
                                             <div class="x_content">
                                                 <p>By verifying your account you can now be able to see wallet & purchase various products that are available at Bit Mine Pool</p>
-                                                <a href="<?php echo BASE_URL.'wallet'?>"><button type="button" class="btn btn-success btn-lg">Verify Now</button></a>
+                                                <a href="<?php echo BASE_URL.'verifyemail'?>"><button type="button" class="btn btn-success btn-lg">Verify Now</button></a>
                                             </div>    
                                                 <?php
                                             } else {
