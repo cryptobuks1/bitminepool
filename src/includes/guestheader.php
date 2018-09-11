7cab9110642b7167434febd4d9bee6a8a80dfca2
unknown <meettomangesh@gmail.com>
2018-09-12 01:10:48 +0530

    To save work

--- src/includes/guestheader.php
+++ src/includes/guestheader.php
@@ -20,10 +20,10 @@
                 </li>
 					
                 <li role="presentation" class="dropdown">
-                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                 <!-- <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-envelope-o"></i>
                     <span class="badge bg-green">0</span>
-                  </a>
+                  </a> -->
                  
                     
                   </ul>
