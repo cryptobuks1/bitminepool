2e4f6b4d0ba22cd9f4bc721c017f3163a5d91805
mangeshn <mangeshn@iprogrammer.com>
2018-09-12 14:13:24 +0530

    To save work

--- src/includes/constant.php
+++ src/includes/constant.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
 #if (!defined('BASE_URL')) define("BASE_URL","http://localhost/bitminepool/new/src/");
-if (!defined('BASE_URL')) define("BASE_URL","https://bitminepool.com/src/");
-if (!defined('QR_BASE_URL')) define("QR_BASE_URL","https://bitminepool.com/");
+if (!defined('BASE_URL')) define("BASE_URL","https://staging.bitminepool.com/src/");
+if (!defined('QR_BASE_URL')) define("QR_BASE_URL","https://staging.bitminepool.com/");
 #if (!defined('API_URL')) define("API_URL","http://localhost:3001/");
-if (!defined('API_URL')) define("API_URL","https://api.bitminepool.com/");
+if (!defined('API_URL')) define("API_URL","https://api-staging.bitminepool.com/");
 if (!defined('ACCESS_TOKEN')) define("ACCESS_TOKEN","3241da18cae0d9ee2cab4ca0633bfa507a56c673");
 
 #if (!defined('DB_SERVER_NAME')) define("DB_SERVER_NAME","localhost");
