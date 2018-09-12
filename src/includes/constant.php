4e7edc918934d534ecefdee78c0505351813d7a7
mangeshn <mangeshn@iprogrammer.com>
2018-09-12 14:22:32 +0530

    To save work

--- src/includes/constant.php
+++ src/includes/constant.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
 #if (!defined('BASE_URL')) define("BASE_URL","http://localhost/bitminepool/new/src/");
-if (!defined('BASE_URL')) define("BASE_URL","https://bitminepool.com/src/");
-if (!defined('QR_BASE_URL')) define("QR_BASE_URL","https://bitminepool.com/");
+if (!defined('BASE_URL')) define("BASE_URL","https://staging.bitminepool.com/src/");
+if (!defined('QR_BASE_URL')) define("QR_BASE_URL","https://staging.bitminepool.com/");
 #if (!defined('API_URL')) define("API_URL","http://localhost:3001/");
-if (!defined('API_URL')) define("API_URL","https://api.bitminepool.com/");
+if (!defined('API_URL')) define("API_URL","https://api-staging.bitminepool.com/");
 if (!defined('ACCESS_TOKEN')) define("ACCESS_TOKEN","3241da18cae0d9ee2cab4ca0633bfa507a56c673");
 
 #if (!defined('DB_SERVER_NAME')) define("DB_SERVER_NAME","localhost");
@@ -12,11 +12,9 @@ if (!defined('ACCESS_TOKEN')) define("ACCESS_TOKEN","3241da18cae0d9ee2cab4ca0633
 #if (!defined('DB_USER_NAME')) define("DB_USER_NAME","root");
 #if (!defined('DB_PASSWORD')) define("DB_PASSWORD","");
 if (!defined('DB_SERVER_NAME')) define("DB_SERVER_NAME","localhost");
-if (!defined('DB_NAME')) define("DB_NAME","apibitmi_bitminepool");
+#if (!defined('DB_NAME')) define("DB_NAME","apibitmi_bitminepool");
+if (!defined('DB_NAME')) define("DB_NAME","apibitmi_bitminepool_staging");
 if (!defined('DB_USER_NAME')) define("DB_USER_NAME","apibitmi_4188294");
 if (!defined('DB_PASSWORD')) define("DB_PASSWORD","emhQo6zE{,Er");
 
-if (!defined('API_KEY')) define("API_KEY","18a11412-eb1b-4b4d-9819-327ed92928c1");
-if (!defined('XPUB')) define("XPUB","xpub6Cnsdhy4K6QQBresDwom1xZZKz6L37MoJ4LSopqC4rirQHBPBmPuqKEZKioYfaB2b7yJorHyrydWtNwKdEFMeNtCga1asTWxp86ut5A62Uk");
-if (!defined('SECRET')) define("SECRET","10081988Mangesh");
 ?>
