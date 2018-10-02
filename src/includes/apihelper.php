fffb938860f5bbc5e56e5f6fe5eeb9d2f7514945
mangeshn <mangeshn@iprogrammer.com>
2018-10-02 19:32:25 +0530

    to save work

--- src/includes/apihelper.php
+++ src/includes/apihelper.php
@@ -1,6 +1,7 @@
 <?php
  // namespace Helper;
-require '../vendor/autoload.php'; 
+if (!defined('PATH')) define("PATH",dirname(__FILE__));
+require_once(PATH . "../vendor/autoload.php");
 include('constant.php');
 class ApiHelper
 {
